a503f6e58e92eb7217cfc914ad67c8cb11db9a02
nakajima97 <kituneudon97@gmail.com>
2025-02-01 18:09:52 +0900

    #1 ボタンを追加して動作することを確認

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main>
       <p className="text-3xl font-bold underline">Hello, world!</p>
+      <Button>Click me</Button>
     </main>
   );
 }
