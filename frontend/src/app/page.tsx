29c284d7ad72405ba1b0531ac75df3e572f273ac
Nakajima <kituneudon97@gmail.com>
2025-02-01 18:11:23 +0900

    shadcn uiを入れた (#2)
    * shadcn/uiが動作することを確認
    
    * #1 ボタンを追加して動作することを確認

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main>
-      <p>Hello, world!</p>
+      <p className="text-3xl font-bold underline">Hello, world!</p>
+      <Button>Click me</Button>
     </main>
   );
 }
