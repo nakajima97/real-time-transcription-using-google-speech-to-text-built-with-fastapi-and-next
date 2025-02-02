e1333525ba6f86c87c7a01c2fef348d1d7b68157
nakajima97 <kituneudon97@gmail.com>
2025-02-02 12:57:45 +0900

    #17 サーバのenvを作成

--- server/api/src/settings/env.py
+++ server/api/src/settings/env.py
@@ -1,5 +1,7 @@
 import os
-
+from dotenv import load_dotenv
 
 def load_env():
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./google-cloud-key.json"
+    load_dotenv()
+    credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_PATH", "./google-cloud-key.json")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
