5800f383ffdc9f2618f625624516132c6aa256ab
nakajima97 <kituneudon97@gmail.com>
2025-02-02 11:03:31 +0900

    #9 shadcnのButtonが動くことを確認

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Button>Click me</Button>
     </main>
   );
 }
