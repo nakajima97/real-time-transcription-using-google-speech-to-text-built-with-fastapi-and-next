9e6199ed66e4cf843b55707119f541609d9b7370
nakajima97 <kituneudon97@gmail.com>
2025-02-02 12:07:29 +0900

    #5 CORS設定

--- server/api/src/main.py
+++ server/api/src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
 from src.websocket.transcription import app_socketio
+from src.setting.cors import add_cors_middleware
 
 app = FastAPI()
-
+add_cors_middleware(app)
 
 @app.get("/")
 def read_root():
