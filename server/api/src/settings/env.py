58d1b35e9bd80bb0607d66d1e73f69b48266215f
Nakajima <kituneudon97@gmail.com>
2025-02-11 10:31:32 +0900

    github actionsの作成 (#29)
    * #25 github actionsの作成
    
    * #25 biomeのエラー解消
    
    * #25 ruffのフォーマッター実行

--- server/api/src/settings/env.py
+++ server/api/src/settings/env.py
@@ -1,10 +1,14 @@
 import os
 from dotenv import load_dotenv
 
+
 def load_env():
     load_dotenv()
-    credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_PATH", "./google-cloud-key.json")
+    credentials_path = os.getenv(
+        "GOOGLE_APPLICATION_CREDENTIALS_PATH", "./google-cloud-key.json"
+    )
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+
 
 def get_env_value(key: str, default: str = None) -> str:
     """
